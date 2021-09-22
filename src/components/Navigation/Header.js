dee0083ca05cf0ddfb6e9f8589b332053a1f7fea
Szymon Rojek <sz.rojek@gmail.com>
2021-09-22 23:02:09 +0100

    uncomment lines of code in Header Navigation

--- src/components/Navigation/Header.js
+++ src/components/Navigation/Header.js
@@ -3,31 +3,32 @@ import { NavLink } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 import "./styles.css";
-
 import Menu from "./Menu";
 
 const Header = () => {
-  // const location = useLocation();
-  // const { pathname } = location;
-  // const splitLocation = pathname.split("/");
+  const location = useLocation();
+  const { pathname } = location;
+  const splitLocation = pathname.split("/");
 
   return (
     <div className="header-container">
       <Menu>
         <ul className="header-list">
           <li
-          // className={`header-link ${splitLocation[1] === "" ? "active" : ""}`}
+            className={`header-link ${splitLocation[1] === "" ? "active" : ""}`}
           >
             <NavLink to="/" className="link">
               Home
             </NavLink>
           </li>
           <li
-          // className={`header-link ${
-          //   splitLocation[1] === "users" ? "active" : ""
-          // }`}
+            className={`header-link ${
+              splitLocation[1] === "users" ? "active" : ""
+            }`}
           >
-            <NavLink className="link">Users</NavLink>
+            <NavLink to="/users" className="link">
+              Users
+            </NavLink>
           </li>
         </ul>
       </Menu>
