57ef47150a8938f2c037ef61a98e83bc371c45fb
Szymon Rojek <sz.rojek@gmail.com>
2021-09-22 23:01:17 +0100

    add index.js do the components dir

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { Error } from "./Error";
+export { Loader } from "./Loader";
+export { Navigation } from "./Navigation";
+export { User } from "./User";
+export { UserContainer } from "./UserContainer";
+export { Map } from "./Map";
